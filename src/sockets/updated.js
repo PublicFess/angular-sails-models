a0266a38c2c29793661dee6141ad4c45f9c31766
Nikita Shuklin <publicfess@gmail.com>
2016-05-12 18:22:20 +0300

    corrected updated

--- src/sockets/updated.js
+++ src/sockets/updated.js
@@ -4,10 +4,14 @@ var assign = require('lodash').assign
 
 var updated = function(data) {
   var self = this;
+  if (self.item && self.item.id == data.id) {
+    assign(self.item, data);
+  }
   if (!self.items || !self.items.length) return;
   var item = _.find(self.items, function(item) {
     return item.id == data.id;
   });
+
   if (item) assign(item, data);
   if (!checkCriteria(data, self.criteriaAll)) {
     var id = findIndex(self.items, {'id': data.id});
